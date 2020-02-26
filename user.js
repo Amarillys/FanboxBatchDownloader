44e1890749917d6af8b17048e78939bd1a363836
Maikaze <i@maikaze.win>
2020-02-26 18:50:28 +0800

    v0.61 Adapt to new Api & UI Improved
    200226
     *    Adapt to new Api! Add Error Tip!
     *    More frequentyle progress bar!
     *    More clearly status!

--- user.js
+++ user.js
@@ -1,9 +1,9 @@
 // ==UserScript==
 // @name         Fanbox Batch Downloader
 // @namespace    http://tampermonkey.net/
-// @version      0.60
+// @version      0.61
 // @description  Batch Download on creator, not post
-// @author       https://github.com/amarillys
+// @author       https://github.com/amarillys QQ 719862760
 // @require      https://cdnjs.cloudflare.com/ajax/libs/jszip/3.2.2/jszip.min.js
 // @require      https://cdnjs.cloudflare.com/ajax/libs/dat-gui/0.7.6/dat.gui.min.js
 // @match        https://www.pixiv.net/fanbox/creator/*
@@ -16,6 +16,10 @@
 
 /**
  * Update Log
+ *  > 200226
+ *    Adapt to new Api! Add Error Tip!
+ *    More frequentyle progress bar!
+ *    More clearly status!
  *  > 200224
  *    More beautiful! UI Redesigned. --use dat.gui,
  *    Performence Improved. -- multi-thread supported.
@@ -62,6 +66,9 @@
 ;(function() {
   'use strict'
 
+  const apiUserUri = 'https://fanbox.pixiv.net/api/creator.get'
+  const apiPostUri = 'https://fanbox.pixiv.net/api/post.listCreator'
+  
   // set style
   GM_addStyle(`
     .dg.main{
@@ -76,7 +83,7 @@
       pointer-events: none;
     }
     .slider-fg {
-      transition: width 0.8s ease-out;
+      transition: width 0.5s ease-out;
     }
   `)
 
@@ -221,8 +228,20 @@
     batch: '分批 / Batch',
     download: '点击这里下载',
     download_en: 'Click to Download',
+    downloading: '下载中...',
+    downloading_en: 'Downloading...',
+    packing: '打包中...',
+    packing_en: 'Packing...',
+    packed: '打包完成',
+    packed_en: 'Packed!',
     init: '初始化中...',
     init_en: 'Initilizing...',
+    initFailed: '请求数据失败',
+    initFailed_en: 'Failed to get Data',
+    initFailed_0: '请检查网络',
+    initFailed_0_en: 'check network',
+    initFailed_1: '或Github联系作者',
+    initFailed_1_en: 'or connect at Github',
     initFinished: '初始化完成',
     initFinished_en: 'Initilized',
     start: '起始 / start',
@@ -233,24 +252,27 @@
     progress: '进度 / Progress',
     speed: '网速 / speed'
   }
+  const EN_FIX = navigator.language.indexOf('zh') > -1 ? '' : '_en'
+
+  let label = null
+  const gui = new dat.GUI({
+    autoPlace: false,
+    useLocalStorage: false
+  })
 
   const clickHandler = {
-    init() {},
+    text() {},
     download: () => {
       console.log('startDownloading')
       downloadByFanboxId(creatorInfo, creatorId)
     },
     pack() {
+      label.name(Text['packing' + EN_FIX])
       zip.pack()
+      label.name(Text['packed' + EN_FIX])
     }
   }
-  const EN_FIX = navigator.language.indexOf('zh') > -1 ? '' : '_en'
-
-  const gui = new dat.GUI({
-    autoPlace: false,
-    useLocalStorage: false
-  })
-  let initText = gui.add(clickHandler, 'init').name(Text['init' + EN_FIX])
+  label = gui.add(clickHandler, 'text').name(Text['init' + EN_FIX])
   let progressCtl = null
 
   let init = async () => {
@@ -259,53 +281,55 @@
     base.appendChild(gui.domElement)
     uiInited = true
 
-    creatorInfo = await getAllPostsByFanboxId(creatorId)
-    initText.name(Text['initFinished' + EN_FIX])
+    try {
+      creatorInfo = await getAllPostsByFanboxId(creatorId)
+      label.name(Text['initFinished' + EN_FIX])
+    } catch (e) {
+        label.name(Text['initFailed' + EN_FIX])
+        gui.add(clickHandler, 'text').name(Text['initFailed_0' + EN_FIX])
+        gui.add(clickHandler, 'text').name(Text['initFailed_1' + EN_FIX])
+        return
+    }
 
     // init dat gui
     const sum = creatorInfo.posts.length
-    progressCtl =  gui.add(options, 'progress', 0, 100, 1).name(Text['progress'])
-    const startCtl = gui.add(options, 'start', 1, sum, 1).name(Text['start'])
-    const endCtl = gui.add(options, 'end', 1, sum, 1).name(Text['end'])
-    endCtl.setValue(sum)
-    gui.add(options, 'thread', 1, 20, 1).name(Text['thread'])
-    gui.add(options, 'batch', 10, 5000, 10).name(Text['batch'])
+    progressCtl = gui.add(options, 'progress', 0, 100, 0.01).name(Text.progress)
+    const startCtl = gui.add(options, 'start', 1, sum, 1).name(Text.start)
+    const endCtl = gui.add(options, 'end', 1, sum, 1).name(Text.end)
+    gui.add(options, 'thread', 1, 20, 1).name(Text.thread)
+    gui.add(options, 'batch', 10, 5000, 10).name(Text.batch)
     gui.add(clickHandler, 'download').name(Text['download' + EN_FIX])
     gui.add(clickHandler, 'pack').name(Text['pack' + EN_FIX])
-
-    startCtl.onChange(() => {
-      if (options.start > options.end)
-        options.start = options.end
-    })
-    endCtl.onChange(() => {
-      if (options.end < options.start)
-        options.end = options.start
-    })
-
+    endCtl.setValue(sum)
+    startCtl.onChange(() => (options.start = options.start > options.end ? options.end : options.start))
+    endCtl.onChange(() => (options.end = options.end < options.start ? options.start : options.end ))
     gui.open()
   }
 
   // init global values
   let zip = null
-  let amount = 0
+  let amount = 1
   let pool = null
+  let progressList = []
   let uiInited = false
+
   const fetchOptions = {
     credentials: 'include',
     headers: {
       Accept: 'application/json, text/plain, */*'
     }
   }
+
+  const setProgress = amount => progressCtl.setValue(progressList.reduce((p, q) => p + q, 0) / amount * 100)
+
   window.onload = () => {
     init()
     let timer = setInterval(() => {
-      if (!uiInited && document.querySelector('.dg.main') === null)
-        init()
-      else clearInterval(timer)
+      (!uiInited && document.querySelector('.dg.main') === null) ? init() : clearInterval(timer)
     }, 3000)
   }
 
-  function gmRequireImage(url) {
+  function gmRequireImage(url, index) {
     return new Promise((resolve, reject) =>
       GM_xmlhttpRequest({
         method: 'GET',
@@ -313,15 +337,30 @@
         overrideMimeType: 'application/octet-stream',
         responseType: 'blob',
         asynchrouns: true,
-        onload: res => resolve(res.response),
-        onprogress: res => console.log(`${res.done} / ${res.total}, ${new Date().getTime()}`),
+        onload: res => {
+          progressList[index] = 1
+          setProgress(amount)
+          resolve(res.response)
+        },
+        onprogress: res => {
+          progressList[index] = res.done / res.total
+          setProgress(amount)
+        },
         onerror: () =>
           GM_xmlhttpRequest({
             method: 'GET',
             url,
             overrideMimeType: 'application/octet-stream',
             responseType: 'arraybuffer',
-            onload: res => resolve(new Blob([res.response])),
+            onload: res => {
+              progressList[index] = 1
+              setProgress(amount)
+              resolve(new Blob([res.response]))
+            },
+            onprogress: res => {
+              progressList[index] = res.done / res.total
+              setProgress(amount)
+            },
             onerror: res => reject(res)
           })
       })
@@ -330,23 +369,25 @@
 
   async function downloadByFanboxId(creatorInfo, creatorId) {
     let processed = 0
-    gui.remove(initText)
+    amount = 1
+    label.name(Text['downloading' + EN_FIX])
     progressCtl.setValue(0)
     let { batch, end, start, thread } = options
     options.progress = 0
-    zip = new Zip(`${creatorId}-${creatorInfo.name}-${start + 1}-${end}`)
+    zip = new Zip(`${creatorId}-${creatorInfo.name}-${start}-${end}`)
     let stepped = 0
-    if (creatorInfo.cover)
-      zip.file('cover.jpg', await gmRequireImage(creatorInfo.cover))
+    creatorInfo.cover ? gmRequireImage(creatorInfo.cover, 0).then(blob => zip.file('cover.jpg', blob)) : null
 
     // init pool
     pool = new ThreadPool(thread)
     pool.finish(() => {
+      label.name(Text['packing' + EN_FIX])
       zip.pack()
+      label.name(Text['packed' + EN_FIX])
     })
 
     // start downloading
-    for (let i = start, p = creatorInfo.posts; i < end; ++i) {
+    for (let i = start - 1, p = creatorInfo.posts; i < end; ++i) {
       let folder = `${p[i].title.replace(/\//g, '-')}-${p[i].id}`
       if (!p[i].body) continue
       let { blocks, imageMap, fileMap, files, images } = p[i].body
@@ -381,9 +422,10 @@
         zip.add(folder, 'article.md', new Blob([article]))
         for (let j = 0; j < imageList.length; ++j) {
           let image = imageList[j]
+          let index = amount
           amount++
           pool.add(() => new Promise((resolve, reject) => {
-            gmRequireImage(image.originalUrl).then(blob => {
+            gmRequireImage(image.originalUrl, index).then(blob => {
               processed++
               zip.add(folder, `${folder}_${j}.${image.extension}`, blob)
               stepped++
@@ -396,9 +438,10 @@
         }
         for (let j = 0; j < fileList.length; ++j) {
           let file = fileList[j]
+          let index = amount
           amount++
           pool.add(() => new Promise((resolve, reject) => {
-            gmRequireImage(file.url).then(blob => {
+            gmRequireImage(file.url, index).then(blob => {
               processed++
               saveBlob(blob, `${creatorId}-${folder}_${j}-${file.name}.${file.extension}`)
               stepped++
@@ -414,9 +457,10 @@
       if (files) {
         for (let j = 0; j < files.length; ++j) {
           let file = files[j]
+          let index = amount
           amount++
           pool.add(() => new Promise((resolve, reject) => {
-            gmRequireImage(file.url).then(blob => {
+            gmRequireImage(file.url, index).then(blob => {
               processed++
               if (blob.size < 51200000)
                 zip.add(folder, `${file.name}.${file.extension}`)
@@ -434,9 +478,10 @@
       if (images) {
         for (let j = 0; j < images.length; ++j) {
           let image = images[j]
+          let index = amount
           amount++
           pool.add(() => new Promise((resolve, reject) => {
-            gmRequireImage(image.originalUrl).then(blob => {
+            gmRequireImage(image.originalUrl, index).then(blob => {
               processed++
               zip.add(folder, `${folder}_${j}.${image.extension}`, blob)
               stepped++
@@ -449,9 +494,8 @@
         }
       }
     }
-    
+    progressList = new Array(amount).fill(0)
     pool.step = () => {
-      progressCtl.setValue(processed / amount * 100)
       console.log(` Progress: ${processed} / ${amount}, Pool: ${pool.running} @ ${pool.sum}`)
       if (stepped >= batch) {
         zip.pack()
@@ -461,17 +505,21 @@
   }
 
   async function getAllPostsByFanboxId(creatorId) {
-    let fristUrl = `https://www.pixiv.net/ajax/fanbox/creator?userId=${creatorId}`
+    // request userinfo
+    const userUri = `${apiUserUri}?userId=${creatorId}`
+    const userData = await (await fetch(userUri, fetchOptions)).json()
     let creatorInfo = {
       cover: null,
       posts: []
     }
-    let firstData = await (await fetch(fristUrl, fetchOptions)).json()
-    let body = firstData.body
-    creatorInfo.cover = body.creator.coverImageUrl
-    creatorInfo.name = body.creator.user.name
-    creatorInfo.posts.push(...body.post.items.filter(p => p.body))
-    let nextPageUrl = body.post.nextUrl
+    const limit = 100
+    creatorInfo.cover = userData.body.coverImageUrl
+    creatorInfo.name = userData.body.user.name
+
+    // request post info
+    let postData = await (await fetch(`${apiPostUri}?userId=${creatorId}&limit=${limit}`, fetchOptions)).json()
+    creatorInfo.posts.push(...postData.body.items.filter(p => p.body))
+    let nextPageUrl = postData.body.nextUrl
     while (nextPageUrl) {
       let nextData = await (await fetch(nextPageUrl, fetchOptions)).json()
       creatorInfo.posts.push(...nextData.body.items.filter(p => p.body))
